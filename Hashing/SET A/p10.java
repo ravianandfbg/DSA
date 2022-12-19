2fc86b59fd0bc71af112cecb76024d5c1dcfac68
Ravi Anand <ravianandfbg@gmail.com>
2022-12-19 21:09:55 +0530

    code

---
+++ Hashing/SET A/p10.java
@@ -0,0 +1,54 @@
+// https://www.geeksforgeeks.org/count-pairs-with-given-sum/
+
+// Input:  arr[] = {1, 5, 7, -1}, sum = 6
+// Output:  2
+// Explanation: Pairs with sum 6 are (1, 5) and (7, -1).
+
+// Input:  arr[] = {10, 12, 10, 15, -1, 7, 6, 5, 4, 2, 1, 1, 1}, sum = 11
+// Output:  9
+// Explanation: Pairs with sum 11 are (10, 1), (10, 1), (10, 1), (12, -1), (10, 1), (10, 1), (10, 1), (7, 4), (6, 5).
+
+// Time : O(N)
+// Space : O(N)
+
+import java.util.*;
+
+class Main{
+    public static int getPairsCount(int[] arr , int n , int sum){
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i = 0 ; i < n ; i++){
+            if(!hm.containsKey(arr[i])){
+                // initializing value to 0, if key not found
+                hm.put(arr[i] , 0);
+            }
+            
+            // if key found
+            hm.put(arr[i] , hm.get(arr[i]) + 1);
+        }
+        
+        int twice_count = 0;
+        // iterate through each element and increment the
+        // count (Notice that every pair is counted twice)
+        for(int i = 0 ; i < n ; i++){
+            if(hm.get(sum-arr[i]) != null){
+                twice_count += hm.get(sum - arr[i]);
+            }
+            // if (arr[i], arr[i]) pair satisfies the
+            // condition, then we need to ensure that the
+            // count is decreased by one such that the
+            // (arr[i], arr[i]) pair is not considered
+            if(sum - arr[i] == arr[i]){
+                twice_count--;
+            }
+        }
+        // return the half of twice_count
+        return twice_count/2;
+    }
+    
+    public static void main (String[] args) {
+        int[] arr = {1, 5, 7, -1, 5};
+        int sum = 6;
+        int n = arr.length;
+        System.out.println(getPairsCount(arr , n , sum));
+    }
+}
---
+++ Hashing/SET A/p9.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/find-common-characters/
+
+// Example 1:
+// Input: words = ["bella","label","roller"]
+// Output: ["e","l","l"]
+
+// Example 2:
+// Input: words = ["cool","lock","cook"]
+// Output: ["c","o"]
+
+// Time : O(N)
+// Space : O(N)
