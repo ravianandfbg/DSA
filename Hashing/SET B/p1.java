ca9f3e4185a48a48e92e5f531f236626ca45c837
Ravi Anand <ravianandfbg@gmail.com>
2022-12-20 16:12:58 +0530

    code

---
+++ Hashing/SET B/p1.java
@@ -0,0 +1,14 @@
+// https://www-geeksforgeeks-org.cdn.ampproject.org/v/s/www.geeksforgeeks.org/number-subarrays-sum-exactly-equal-k/amp/?amp_js_v=a6&amp_gsa=1&usqp=mq331AQKKAFQArABIIACAw%3D%3D#aoh=16470976960317&referrer=https%3A%2F%2Fwww.google.com&amp_tf=From%20%251%24s&ampshare=https%3A%2F%2Fwww.geeksforgeeks.org%2Fnumber-subarrays-sum-exactly-equal-k%2F
+
+// Input : arr[] = {10, 2, -2, -20, 10}, k = -10
+// Output : 3
+// Explanation: Subarrays: arr[0…3], arr[1…4], arr[3..4] have a sum exactly equal to -10.
+
+// Input : arr[] = {9, 4, 20, 3, 10, 5}, k = 33
+// Output : 2
+// Explanation: Subarrays : arr[0…2], arr[2…4] have a sum exactly equal to 33.
+
+// Time : O(N)
+// Space: O(N)
+
+
