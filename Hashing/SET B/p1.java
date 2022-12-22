fd1714e36ecbb70c1f0469fa451a9cdcb263716c
Ravi Anand <ravianandfbg@gmail.com>
2022-12-22 09:02:53 +0530

    code

--- Hashing/SET B/p1.java
+++ Hashing/SET B/p1.java
@@ -1,5 +1,5 @@
 // https://www-geeksforgeeks-org.cdn.ampproject.org/v/s/www.geeksforgeeks.org/number-subarrays-sum-exactly-equal-k/amp/?amp_js_v=a6&amp_gsa=1&usqp=mq331AQKKAFQArABIIACAw%3D%3D#aoh=16470976960317&referrer=https%3A%2F%2Fwww.google.com&amp_tf=From%20%251%24s&ampshare=https%3A%2F%2Fwww.geeksforgeeks.org%2Fnumber-subarrays-sum-exactly-equal-k%2F
-
+// Leetcode 560
 // Input : arr[] = {10, 2, -2, -20, 10}, k = -10
 // Output : 3
 // Explanation: Subarrays: arr[0…3], arr[1…4], arr[3..4] have a sum exactly equal to -10.
@@ -10,5 +10,4 @@
 
 // Time : O(N)
 // Space: O(N)
-
 
