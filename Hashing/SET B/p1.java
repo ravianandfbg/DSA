7df727913d1021646a6a1185320215f06e49dc6c
Ravi Anand <ravianandfbg@gmail.com>
2022-12-22 20:10:52 +0530

    code

--- Hashing/SET B/p1.java
+++ Hashing/SET B/p1.java
@@ -1,5 +1,7 @@
 // https://www-geeksforgeeks-org.cdn.ampproject.org/v/s/www.geeksforgeeks.org/number-subarrays-sum-exactly-equal-k/amp/?amp_js_v=a6&amp_gsa=1&usqp=mq331AQKKAFQArABIIACAw%3D%3D#aoh=16470976960317&referrer=https%3A%2F%2Fwww.google.com&amp_tf=From%20%251%24s&ampshare=https%3A%2F%2Fwww.geeksforgeeks.org%2Fnumber-subarrays-sum-exactly-equal-k%2F
 // Leetcode 560
+// Pepcoding(HashMap Level 2 - Q9) -> https://classroom.pepcoding.com/myClassroom/the-placement-program-pitampura-apr-19-2021/hashmap-and-heap-level-2/count-of-subarrays-having-sum-equals-to-k-official/ojquestion
+
 // Input : arr[] = {10, 2, -2, -20, 10}, k = -10
 // Output : 3
 // Explanation: Subarrays: arr[0…3], arr[1…4], arr[3..4] have a sum exactly equal to -10.
---
+++ Hashing/SET B/p2.java
@@ -0,0 +1,5 @@
+// https://stackoverflow.com/questions/64418218/smallest-subarray-with-sum-equal-to-k
+
+// Input: arr[] = {2, 4, 6, 10, 2, 1}, K = 12 
+// Output: 2
+// Explanation: All possible subarrays with sum 12 are {2, 4, 6} and {10, 2}.
